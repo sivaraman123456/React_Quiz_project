1f5bc155a62ee026113625e8403879360c0f81ea
Premkumar <premkumar.ashokan@talentship.io>
2024-12-15 13:10:35 +0530

    start project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-xl text-blue-600 text-center font-bold flex justify-center items-center h-[100vh]'>Hello </h1>
+    </>
+  )
+}
+
+export default App
